7e483b777b438d4b51c6be717682e748b2d4e448
PAandW <paandw@gmail.com>
2021-06-08 20:43:33 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/paandw/apps/drinking_games/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.paandw.apps.drinking_games
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/res/values/colors.swift
@@ -0,0 +1,8 @@
+let primaryColor = UIColor(red: 0.29, green: 0.28, blue: 0.28, alpha: 1.0);
+let primaryLightColor = UIColor(red: 0.45, green: 0.45, blue: 0.45, alpha: 1.0);
+let primaryDarkColor = UIColor(red: 0.13, green: 0.13, blue: 0.13, alpha: 1.0);
+let primaryTextColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.0);
+let secondaryColor = UIColor(red: 0.71, green: 0.74, blue: 0.58, alpha: 1.0);
+let secondaryLightColor = UIColor(red: 0.91, green: 0.93, blue: 0.77, alpha: 1.0);
+let secondaryDarkColor = UIColor(red: 0.53, green: 0.55, blue: 0.40, alpha: 1.0);
+let secondaryTextColor = UIColor(red: 0.16, green: 0.25, blue: 0.07, alpha: 1.0);
